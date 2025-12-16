c15a8349a5ad781b00d05f55a397ef5fe94171f6
Melvin Chia <melvinchia623600@gmail.com>
2025-12-16 09:39:14 +0800

    chore: first commit

---
+++ client/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <main className="flex items-center justify-center min-h-dvh bg-zinc-900 text-zinc-100 text-2xl tracking-widest">
+      Make Hackathon Great Again!
+    </main>
+  );
+}
+
+export default App;
