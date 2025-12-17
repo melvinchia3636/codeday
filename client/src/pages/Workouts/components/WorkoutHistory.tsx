43e86390a62123114c2590ec1c70b05bde7e1958
Melvin Chia <melvinchia623600@gmail.com>
2025-12-17 17:54:03 +0800

    -

--- client/src/pages/Workouts/components/WorkoutHistory.tsx
+++ client/src/pages/Workouts/components/WorkoutHistory.tsx
@@ -81,7 +81,7 @@ export function WorkoutHistory() {
     const [datePart, timePart] = dateStr.split(" ");
     const [year, month, day] = datePart.split("-").map(Number);
     const [hour, minute] = timePart ? timePart.split(":").map(Number) : [0, 0];
-    
+
     const date = new Date(year, month - 1, day, hour, minute);
     return date.toLocaleString("en-US", {
       month: "short",
